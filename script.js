1956ba2c3d9e5076cefc4b66bf92fefbf32b01a9
Studdnet <st.codding@gmail.com>
2023-01-05 23:21:21 +0300

    feat: add lenghtSlider

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict"; 
 
+const lengthSlider = document.querySelector(".pass-length input"),
+		generateBtn = document.querySelector(".generate-btn");
+
+const generatePassword = () => {
+	
+};
+
+const updateSlider = () => {
+	//passing slider value as counter text
+	document.querySelector(".pass-length span").innerHTML = lengthSlider.value;
+};
+
+updateSlider();
+
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
